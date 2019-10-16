4671db57e0638a7d18871400a126c723a1307d7b
Charles Shuller <charles.shuller@gmail.com>
2019-10-16 16:38:45 -0500

    Updated the License from default to LGPL 3 or Later

--- src/Maybe.ts
+++ src/Maybe.ts
@@ -1,6 +1,26 @@
 /**
  * Main module for the Maybe type.
+ *
+ * Copyright 2019 Charles Shuller
+ *
+ * This file is part of fs-maybe.
+ *
+ * fs-maybe is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * fs-maybe is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with fs-maybe.  If not, see <https://www.gnu.org/licenses/>.
+ *
  */
+
+
 
 /**
  * Type used to define a Maybe.  A Maybe is a _Just or _Nothing, but
